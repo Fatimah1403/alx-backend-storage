c20e50381740beec68606bf634c7bdbfeee08971
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-17 16:28:09 -0700

    Task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Python function that changes all topics of
+    a school document based on the name:mongo_collection
+    will be the pymongo collection object name
+    (string) will be the school name to updatetopics
+    list of strings) will be the list of topics approached in the school
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    topics (list of strings) will be the
+    list of topics approached in the school
+    """
+    alltopics = mongo_collection.update_many({"name": "name"},
+                                             {$"set": {"topics": topics}})
+    return alltopics
