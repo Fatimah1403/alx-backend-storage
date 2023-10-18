5ea102d77d32d1d6ae3b265d526630d0caedb14a
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-18 22:48:26 +0000

    writing string to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,8 +10,10 @@ Type-annotate store correctly. Remember that data
 can be a str, bytes, int or float.
 
 """
+from uuid import uuid4
 import redis
 import uuid
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -19,10 +21,16 @@ class Cache:
     that data can be a str, bytes, int or float.
     """
     def __init__(self):
+        """
+        initialize a cache instance
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str,bytes, int, float]) -> str:
+        """
+        store a value in redis
+        """
         key = str(uuid.uuid4())
 
         self._redis.set(key, data)
