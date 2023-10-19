838a5516972bb85eea2e7baaf63afd539c45fe67
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-19 00:17:40 +0000

    expiring web cache

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+In this tasks, we will implement a get_page function
+(prototype: def get_page(url: str) -> str:). The core of the
+function is very simple. It uses the requests module to obtain
+the HTML content of a particular URL and returns it.
+Start in a new file named web.py and do not reuse the code written in exercise.py.
+Inside get_page track how many times a particular URL was accessed in the key
+"count:{url}" and cache the result with an expiration time of 10 seconds.
+Tip: Use http://slowwly.robertomurray.co.uk to simulate a slow
+response and testyour caching.
+Bonus: implement this use case with decorators.
+"""
+
+import redis
+import requests
+from functools import wraps
+
+redis_store = redis.Redis()
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
